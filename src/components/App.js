fa2f19b093fd442edffe2cacf9de6eb7c2d82a51
NehaNayak123 <111532433+NehaNayak123@users.noreply.github.com>
2025-01-21 21:37:09 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 
 import React from "react";
 import './../styles/App.css';
-
+import {useState, useEffect} from "react"
 const App = () => {
+  const [data, setData]=useState(null);
+  useEffect(()=>{
+    fetch("https://dummyjson.com/products")
+    .then(res=>res.json())
+    .then(data=>setData(data))
+    .catch(err=>setData("Error"))
+  },[])
+  if(data==="Error") return <p>Error Occured</p>
+  if(!data) return <h1>Loading...</h1>
   return (
     <div>
         {/* Do not remove the main div */}
+      <pre>{JSON.stringify(data, null 2)}</pre>
     </div>
   )
 }
